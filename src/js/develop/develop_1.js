0e16f8c324180cf862330bc09a2fc43d1f8b98e9
nickolay Gotsliyk <trolizmaslom@gmail.com>
2016-05-26 10:26:59 +0300

    qwe

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -8,14 +8,42 @@ function butter(){
        }
     });
 }
+function changeViewport(){
+    $(window).resize(function(){
+        whatViewport();
+    });
+    function whatViewport(){
+        var windowWidth = screen.width;
+        var viewport = $("meta[name=viewport]");
+        viewport.attr('content', 'width=device-width');
+        if(windowWidth<=640){
+            viewport.attr('content', 'width=640');
+        }
+    };
 
+}
 
+var canvas1, stage1, exportRoot1;
+
+function init1() {
+    canvas1 = document.getElementById("canvas1");
+    exportRoot1 = new lib1.logo_index();
+
+    stage1 = new createjs.Stage(canvas1);
+    stage1.addChild(exportRoot1);
+    stage1.update();
+
+    createjs.Ticker.setFPS(lib1.properties.fps);
+    createjs.Ticker.addEventListener("tick", stage1);
+}
 $(document).ready(function(){
     butter();
 });
 
 $(window).load(function(){
-
+    init1();
+    changeViewport();
+    $('body').attr('style','opacity:1');
 });
 
 $(window).resize(function(){
